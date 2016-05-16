e4ae646ff3d3358666a1305d8a94a25df41fb4ce
Kevin <ujs_ck@163.com>
2016-05-16 12:58:38 +0800

    Add HistogramChartWPF

---
+++ Halcon Toolkit/Image/Acquisition/FrameGrabber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcon_Toolkit.Image.Acquisition
+{
+    public class FrameGrabber
+    {
+        public static void OpenFrameGrabber()
+        {
+            IFrameGrabber camera;
+            //
+        }
+    }
+}
